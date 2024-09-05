f66c44ce0bbed0cd109e90375a70c8776f41d1fa
Kevin <kevin.lhuillier@outlook.fr>
2024-09-05 07:10:18 +0200

    Test

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-      res.status(200).json("Hello CI/CD");
+      res.status(200).json("Hello CI/CD self updated!");
 });
 
 app.listen(80);
